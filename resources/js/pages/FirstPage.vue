ec91d41f64e839d08ffd76ceca8c67f0bf1e8598
Okan Ozbek <devokan@outlook.com>
2023-12-09 22:17:19 +0100

    Removed line

--- resources/js/pages/FirstPage.vue
+++ resources/js/pages/FirstPage.vue
@@ -2,7 +2,6 @@
     <div class="flex flex-col justify-center items-center">
         <h1 class="mb-8 text-3xl font-bold"> Hello world </h1>
         <p> This is the first text written in Vue & Inertia (on this project). </p>
-        <p class="mb-4"> The following text is written from the controller.. </p>
         <p> Text in question: <span class="font-italic"> {{ message }} </span> </p>
     </div>
 </template>
